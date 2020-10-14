ff331848a86dededa766ef9954cc6ee6d2f0bfa0
zhao_hp <1422374523@qq.com>
2020-10-14 17:22:26 +0800

    src

---
+++ src/main/java/life/zhao/community/community/CommunityApplication.java
@@ -0,0 +1,21 @@
+package life.zhao.community.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+    }
+
+}
+
+//echo "# community" >> README.md
+//        git init
+//        git add README.md
+//        git commit -m "first commit"
+//        git branch -M main
+//        git remote add origin https://github.com/zhaozhengcycle/community.git
+//        git push -u origin main
